fef413e60522b9ce795170026a602d2b92bf3b85
Heather You <heather.fswd@gmail.com>
2019-05-24 15:54:53 -0400

    Display an empty board

--- Tests/BoardTest.cs
+++ Tests/BoardTest.cs
@@ -1,10 +1,21 @@
 using NUnit.Framework;
+using TicTacToe;
 
 namespace Tests
 {
     [TestFixture]
-    public class Board
+    public class BoardTest
     {
-        
+        [Test]
+        public void DisplaysAnEmptyBoard()
+        {
+            MockConsoleWriter writer = new MockConsoleWriter();
+            Board board = new Board(writer);
+
+            board.DisplayBoard();
+            var expected = @" 1 | 2 | 3  ===+===+===  4 | 5 | 6  ===+===+===  7 | 8 | 9 ";
+
+            Assert.AreEqual(expected, writer.LastOutput );
+        }
     }
 }
